a0e98c273e9a121ed07a397ada308891318f3320
Gabriel Amaral <gabriel.andrade.gbi@valor.com.br>
2025-03-21 12:09:02 -0300

    feat(frontend): projeto inicial

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+import './globals.css';
+
+export const metadata: Metadata = {
+	title: 'Portifolio - Gabriel Amarl',
+	description: 'Portifolio de projetos de desenvolvimento',
+};
+
+const font = Montserrat({
+	subsets: ['latin'],
+});
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="pt-BR">
+			<body className={`${font.className} antialiased`}>{children}</body>
+		</html>
+	);
+}
